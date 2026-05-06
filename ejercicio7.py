7d0a5848d4ee95ac92681ecf0863f5c72272481c
sosaa2709 <drkr50sosa@gmail.com>
2026-05-06 11:30:15 -0300

    Add files via upload

---
+++ ejercicio7.py
@@ -0,0 +1,6 @@
+print ("Ingrese 2 numeros y se le mostrara el promedio de ambos")
+N1=int(input("Ingrese el primer numero "))
+N2=int(input("Ingrese el segundo numero "))
+SS=N1+N2
+PP=SS/2
+print ("El promedio de las notas es: ", PP)
