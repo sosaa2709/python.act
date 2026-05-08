20b4def91af6779511b2d4612bd0bd56b8a9c785
sosaa2709 <drkr50sosa@gmail.com>
2026-05-08 09:26:16 -0300

    hola

--- act8/ejercicio2.pyy.py
+++
@@ -1,4 +0,0 @@
-print ("Ingrese el precio total y se le indicara cual es el precio final incluida la propina")
-TOT=float(input())
-FIN= TOT *1.10
-print ("El precio final es: ", FIN)
---
+++ act_dia8/ejercicio3.pyy.py
@@ -0,0 +1,5 @@
+print ("Ingrese las horas y los minutos del viaje para calcular el tiempo total(los minutos ingresarlos con 0.0)")
+HOR=int(input())
+MIN=float(input())
+TOT= HOR+MIN
+print ("El tiempo total del viaje es: ", TOT)
