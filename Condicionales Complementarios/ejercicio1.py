ed175090736c691bcfe4b52910db3975d2f89f91
sosaa2709 <drkr50sosa@gmail.com>
2026-05-08 10:53:50 -0300

    Create ejercicio1.py

---
+++ Condicionales Complementarios/ejercicio1.py
@@ -0,0 +1,8 @@
+print ("Ingrese tres numeros y se determinara si son iguales o diferentes")
+a=int(input())
+b=int(input())
+c=int(input())
+if a==b and a==c:
+    print ("Son iguales")
+else:
+    print ("Son diferentes")
