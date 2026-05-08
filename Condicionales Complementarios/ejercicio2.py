6e085c5918e89e446475bd873e91824e3e0408f6
sosaa2709 <drkr50sosa@gmail.com>
2026-05-08 11:02:32 -0300

    Create ejercicio2.py

---
+++ Condicionales Complementarios/ejercicio2.py
@@ -0,0 +1,7 @@
+print ("Ingrese dos numeros y se mostraran en orden de menor a mayor")
+a=int(input())
+b=int(input())
+if a<b: 
+    print ("Los números son: ", a, b)
+else:
+    print ("Los números son: ", b, a)
