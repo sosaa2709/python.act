c1c357bc9cf7e08f69de5ffd10d0890af82fc3d2
sosaa2709 <drkr50sosa@gmail.com>
2026-05-20 11:33:55 -0300

    Update ejercicio1.py

--- 2da tanda de condicionales/ejercicio1.py
+++ 2da tanda de condicionales/ejercicio1.py
@@ -1,8 +1,8 @@
 print ("Ingrese la temperatura corporal (C°) y se clasificara")
-tem=int(input())
+tem=float(input())
 if tem<36:
     print ("Hipotermia")
-elif tem>=36 and tem<=37:
+elif tem>=36 and tem<=37.5:
     print ("Normal")
 else:
     print ("Fiebre")
